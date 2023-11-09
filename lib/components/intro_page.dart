2f6b7a209243eea3626bb3d910523547975da997
Cao Nam <nam52871@donga.edu.vn>
2023-11-09 10:29:08 +0700

    Create component introPage

---
+++ lib/components/intro_page.dart
@@ -0,0 +1,106 @@
+import 'package:fitness_app/configs/app_icons.dart';
+import 'package:fitness_app/pages/onboarding_page.dart';
+import 'package:fitness_app/styles/app_colors.dart';
+import 'package:fitness_app/styles/app_styles.dart';
+import 'package:fitness_app/styles/app_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:widget_mask/widget_mask.dart';
+
+class IntroPage extends StatelessWidget {
+  final String title;
+  final String desc;
+  final int indexPage;
+  final controller;
+  final Widget image;
+
+  IntroPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.desc,
+    this.controller,
+    required this.indexPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(child: image),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: AppStyles.PaddingBothSidesPage),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: AppText.heading2.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  desc,
+                  style: AppText.medium.copyWith(
+                      fontWeight: FontWeight.w400, color: AppColors.gray_1),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: FloatingActionButton.large(
+        focusElevation: 0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: CircularProgressIndicator(
+                  color: AppColors.primary,
+                  value: indexPage * (1 / listIntroPageItem.length),
+                  strokeWidth: 2,
+                ),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                left: 5,
+                bottom: 5,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      gradient: AppColors.primaryGradiant,
+                      borderRadius: BorderRadius.all(Radius.circular(100))),
+                  child: SvgPicture.asset(
+                    AppIcons.ic_arrow_right,
+                    width: 10,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        onPressed: () {
+          if (controller.hasClients) {
+            controller.jumpToPage(
+              indexPage,
+            );
+          }
+        },
+      ),
+    );
+  }
+}
