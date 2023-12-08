e07614647e54f5aaeedd6dc5c05efd90d0c0ebf0
Cao Nam <nam52871@donga.edu.vn>
2023-12-08 07:47:07 +0700

    Create: list lastest activity

--- lib/pages/dashboard/activityTracker/index.dart
+++ lib/pages/dashboard/activityTracker/index.dart
@@ -1,5 +1,7 @@
+import 'package:fitness_app/components/title_section.dart';
 import 'package:fitness_app/components/tool_bar.dart';
 import 'package:fitness_app/pages/dashboard/activityTracker/widgets/banner_activity_tracker.dart';
+import 'package:fitness_app/pages/dashboard/activityTracker/widgets/last_avtivity_card.dart';
 import 'package:fitness_app/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -13,15 +15,82 @@ class ActivityTracker extends StatelessWidget {
       body: Padding(
         padding:
             EdgeInsets.symmetric(horizontal: AppStyles.paddingBothSidesPage),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 30,
+              ),
             ),
-            BannerActivityTracker()
+            SliverToBoxAdapter(
+              child: BannerActivityTracker(),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 20,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: TitleSection(
+                title: 'Latest Activity',
+                onPressed: () {},
+              ),
+            ),
+            SliverList.separated(
+              itemCount: listActivityTracker.length,
+              itemBuilder: (context, index) {
+                return LastActivityCard();
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 20);
+              },
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class ActivityTrackerClass {
+  String title;
+  String desc;
+  String image;
+
+  ActivityTrackerClass({
+    required this.title,
+    required this.desc,
+    required this.image,
+  });
+}
+
+List<ActivityTrackerClass> listActivityTracker = [
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Fullbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout'),
+  ActivityTrackerClass(
+      desc: '180 Calories Burn | 20minutes',
+      image: '',
+      title: 'Lowerbody Workout')
+];
--- lib/styles/app_shadows.dart
+++ lib/styles/app_shadows.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppShadows {
   static const shadowCard = BoxShadow(
-    color: Color.fromARGB(95, 179, 189, 250),
-    spreadRadius: 0,
+    color: Color.fromARGB(95, 229, 231, 241),
+    spreadRadius: 4,
     blurRadius: 1,
-    offset: Offset(0, 1), // changes position of shadow
+    offset: Offset(0, 2), // changes position of shadow
   );
 }
